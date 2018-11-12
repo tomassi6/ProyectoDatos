56d76b6908457588dd115cca111d42becee680fe
Tomas Navarro <tomas-navarro@hotmail.com>
2018-11-12 13:55:49 -0500

    Proyecto Final V1

--- Main.java
+++ Main.java
@@ -6,135 +6,129 @@ import java.util.ArrayList;
 public class Main {
 
     /*
-      Este metodo es para convertir el arrayList de float[] a un arreglo de float[], osea una matriz
-      de tipo float.
+      Este metodo es para convertir el arrayList de float[] a un arreglo de DroneBee[]
      */
 
-    public static float[][] convertToArray(ArrayList<float[]> arrayList){
+    public static DroneBee[] convertToArray(ArrayList<double[]> arrayList){
 
-        float[][] finArr = new float[arrayList.size()][3];
+        DroneBee[] listDrone = new DroneBee[arrayList.size()];
 
         for (int i = 0; i < arrayList.size(); i ++){
-            float[] temp = arrayList.get(i);
-            finArr[i][0] = temp[0];
-            finArr[i][1] = temp[1];
-            finArr[i][2] = temp[2];
+            double[] temp = arrayList.get(i);
+            DroneBee bee = new DroneBee(temp[0],temp[1],temp[2]);
+            listDrone[i] = bee;
         }
 
-        return finArr;
+        return listDrone;
 
     }
 
     /*
-     Aca usamos el algoritmo quickSort o cualquiera que sea mas rapido.. para organizar la matriz por la
+     Aca usamos el algoritmo RadixSort para organizar la matriz por la
      posicion x de lar coordenadas
      */
 
-      
-   
     //Obtener bytes de long
     public static byte[] getBytes(long v) {
-     byte[] writeBuffer = new byte[8];
-     writeBuffer[7] = (byte) ((v >>> 56) & 0xFF);
-     writeBuffer[6] = (byte) ((v >>> 48) & 0xFF);
-     writeBuffer[5] = (byte) ((v >>> 40) & 0xFF);
-     writeBuffer[4] = (byte) ((v >>> 32) & 0xFF);
-     writeBuffer[3] = (byte) ((v >>> 24) & 0xFF);
-     writeBuffer[2] = (byte) ((v >>> 16) & 0xFF);
-     writeBuffer[1] = (byte) ((v >>> 8) & 0xFF);
-     writeBuffer[0] = (byte) ((v >>> 0) & 0xFF);
-     return writeBuffer;
-   }
+        byte[] writeBuffer = new byte[8];
+        writeBuffer[7] = (byte) ((v >>> 56) & 0xFF);
+        writeBuffer[6] = (byte) ((v >>> 48) & 0xFF);
+        writeBuffer[5] = (byte) ((v >>> 40) & 0xFF);
+        writeBuffer[4] = (byte) ((v >>> 32) & 0xFF);
+        writeBuffer[3] = (byte) ((v >>> 24) & 0xFF);
+        writeBuffer[2] = (byte) ((v >>> 16) & 0xFF);
+        writeBuffer[1] = (byte) ((v >>> 8) & 0xFF);
+        writeBuffer[0] = (byte) ((v >>> 0) & 0xFF);
+        return writeBuffer;
+    }
 
-    //convertir a long
     public static long toLong(byte[] data, int offset) {
         return ((long)(data[offset + 7] & 0xFF) << 56) | ((long)(data[offset + 6] & 0xFF) << 48)
-            | ((long)(data[offset + 5] & 0xFF) << 40)
-            | ((long)(data[offset + 4] & 0xFF) << 32)  | ((long)(data[offset + 3] & 0xFF) << 24)  | ((long)(data[offset + 2] & 0xFF) << 16)  | ((long)(data[offset + 1] & 0xFF) << 8)  | ((long)(data[offset] & 0xFF));
+                | ((long)(data[offset + 5] & 0xFF) << 40)
+                | ((long)(data[offset + 4] & 0xFF) << 32)  | ((long)(data[offset + 3] & 0xFF) << 24)  | ((long)(data[offset + 2] & 0xFF) << 16)  | ((long)(data[offset + 1] & 0xFF) << 8)  | ((long)(data[offset] & 0xFF));
     }
 
     //Radix Sort
 
-    public static double[] RadixSort(double[] array){
+    public static void RadixSort(DroneBee[] array){
 
 
-    // array temporales para convertir de double a long
-        long[] t = new long[array.length];
-        long[] a = new long[array.length];
+        // array temporales para convertir de float a int
+        DroneBee[] temp = new DroneBee[array.length];
+
         for(int i = 0; i < array.length; i++){
-                byte[] x = getBytes(Double.doubleToRawLongBits(array[i]));
-                a[i] = toLong(x, 0);
+            byte[] x = getBytes(Double.doubleToRawLongBits(array[i].getPos_fx()));
+            array[i].setPos_x(toLong(x,0));
 
         }
 
-    int groupLength = 8;
-    int bitLength = 64;
+        int groupLength = 8;
+        int bitLength = 64;
 
-    // dimension de 2^r, numero de valores posibles de un número de r bits
-    long[] count = new long[1 << groupLength];
-    long[] pref = new long[1 << groupLength];
-    int groups = bitLength / groupLength;
-    int mask = (1 << groupLength) - 1;
-    int negatives = 0, positives = 0;
-    int shift = 0;
-     for(int c = 0; c < groups; c++, shift += groupLength)
-    {
-        // reset array count
-        for(int j = 0; j < count.length; j++){
-            count[j] = 0;
-        }
+        // dimension de 2^r, numero de valores posibles de un número de r bits
+        int[] count = new int[1 << groupLength];
+        int[] pref = new int[1 << groupLength];
+        int groups = bitLength / groupLength;
+        int mask = (1 << groupLength) - 1;
+        int negatives = 0, positives = 0;
+        int shift = 0;
 
-        for(int i = 0; i < a.length; i++)
+        for(int c = 0; c < groups; c++, shift += groupLength)
         {
-            count[(int)(a[i] >> shift) & mask]++;
-
-            // Cuenta todos los valores negativos en primera ronda
-            if(c == 0 && a[i] < 0)
-                negatives++;
-        }
-        if(c == 0){
-            positives = a.length - negatives;
-        }
-        // calcula los prefijos
-        pref[0] = 0;
-        for(int i = 1; i < count.length; i++){
-                 pref[i] = pref[i - 1] + count[i - 1];
-        }
-        // ordena elementos de a[] a t[]
-        for(int i = 0; i < a.length; i++){
-            // Obtiene índice para ordenar el número
-            int index = (int) pref[(int)(a[i] >> shift) & mask]++;
-
-            if(c == groups - 1)
-            {
-                // Último grupo (de los más significativos), si el número es negativo se ordena inversamente al frente de la matriz, dejando los positivos al fondo
-                if(a[i] < 0){
-                    index = positives - (index - negatives) - 1;
-                }else{
-                    index += negatives;
-                }
+            // reset array count
+            for(int j = 0; j < count.length; j++){
+                count[j] = 0;
             }
-            t[index] = a[i];
+
+            for(int i = 0; i < array.length; i++)
+            {
+                count[(int)(array[i].getPos_x() >> shift) & mask]++;
+
+                // Cuenta todos los valores negativos en primera ronda
+                if(c == 0 && array[i].getPos_x() < 0)
+                    negatives++;
+            }
+            if(c == 0){
+                positives = array.length - negatives;
+            }
+            // calcula los prefijos
+            pref[0] = 0;
+            for(int i = 1; i < count.length; i++){
+                pref[i] = pref[i - 1] + count[i - 1];
+            }
+            // ordena elementos de a[] a t[]
+            for(int i = 0; i < array.length; i++){
+                // Obtiene índice para ordenar el número
+                int index = pref[(int)(array[i].getPos_x() >> shift) & mask]++;
+
+                if(c == groups - 1)
+                {
+                    // Último grupo (de los más significativos), si el número es negativo se ordena inversamente al frente de la matriz, dejando los positivos al fondo
+                    if(array[i].getPos_x() < 0){
+                        index = positives - (index - negatives) - 1;
+                    }else{
+                        index += negatives;
+                    }
+                }
+                temp[index] = array[i];
+            }
+
+            // a[] = t[], comienza de nuevo hasta el último grupo
+            array = temp.clone();
         }
 
-        // a[] = t[], comienza de nuevo hasta el último grupo
-        a = t.clone();
+
+        /*
+            //Este Codigo es para saber si el algoritmo los esta ordenando.
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Posición ["+i+"] = "+ array[i].getPos_fx());
+        }
+
+        */
+
     }
 
-    // Convierte long a la matriz double de nuevo
-    double[] ret = new double[a.length];
-    for(int i = 0; i < a.length; i++){
-        long y = toLong(getBytes(a[i]), 0);
-        ret[i] = Double.longBitsToDouble(y);
-        System.out.println("Posición ["+i+"] = "+ ret[i]);
-    }
-      return ret;
-    }
-
-
-    
-   
-    
 
     /*
       Aca iria el metodo donde comparamos las coordenadas entre si para determinar cuales estan a menos
@@ -142,15 +136,62 @@ public class Main {
 
      */
 
-    public static void compararPos(){
 
+    public static boolean calculateDistanceLong(double xA, double xB){
+        return Math.abs(xB - xA) * 111111 <= 100;
+    }
+
+
+    public static boolean calculateDistanceNormal(double xA, double xB, double yA, double yB, double zA, double zB){
+
+        return Math.sqrt(  Math.pow((xA - xB)*111111,2) +
+                           Math.pow((yA - yB)*111111,2) +
+                           Math.pow(zA - zB,2) ) <= 100;
 
     }
+
+    public static void compararPos(DroneBee[] array){
+
+        //Este metodo G.O.A.T...
+
+        for (int i = 0; i < array.length - 1; i++ ){
+
+            int j = i + 1;
+
+            double t = array[i].getPos_fx();
+            double u = array[j].getPos_fx();
+
+            double v = array[i].getPos_fy();
+
+            double x = array[i].getPos_fz();
+
+            while(calculateDistanceLong(t, u) && j < array.length){
+
+                u = array[j].getPos_fx();
+                double w = array[j].getPos_fy();
+                double y = array[j].getPos_fz();
+
+                if(calculateDistanceNormal(t, u, v, w, x, y)){
+                    System.out.println("Abejas en riezgo de colision");
+                    System.out.println("Abeja 1: " + "," + t + "," + v + "," + x);
+                    System.out.println("Abeja 2: " + "," + u + "," + w + "," + y);
+                    System.out.println("------------------------------------------");
+                }
+                j++;
+            }
+
+        }
+
+    }
+
+
 
     public static void main(String[] args){
 
         Reader readFile = new Reader();
         Scanner console = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del archivo......... no ponga el .txt");
         String fileName = console.nextLine();
 
         try {
@@ -159,9 +200,12 @@ public class Main {
             e.printStackTrace();
         }
 
-        ArrayList<float[]> temp = readFile.getList();
+        ArrayList<double[]> temp = readFile.getList();
 
-        float[][] array = convertToArray(temp);
+        DroneBee[] array = convertToArray(temp);
+        RadixSort(array);
+
+        compararPos(array);
 
     }
 
--- Reader.java
+++ Reader.java
@@ -5,31 +5,30 @@ import java.util.ArrayList;
 
 public class Reader {
 
-    private ArrayList<float[]> mat = new ArrayList<>();
+    private ArrayList<double[]> mat = new ArrayList<>();
 
     public void leerArchivo(String file) throws FileNotFoundException{
         Scanner in = new Scanner(new File(file+".txt"));
 
         String visaje_linea = in.nextLine(); // La primera line no sirve pa nada
 
-        int cont = 0;
 
+        int cont = 0;
         while(in.hasNextLine()){
-            float[] pos = new float[3];
+            double[] pos = new double[3];
 
             String line = in.nextLine();
             String[] tokens = line.split(",");
 
-            pos[0] = Float.parseFloat(tokens[0]); //pos_x
-            pos[1] = Float.parseFloat(tokens[1]); //pos_y
-            pos[2] = Float.parseFloat(tokens[2]); //pos_z
-
+            pos[0] = Double.parseDouble(tokens[0]); //pos_x
+            pos[1] = Double.parseDouble(tokens[1]); //pos_y
+            pos[2] = Double.parseDouble(tokens[2]); //pos_z
             mat.add(pos);
 
         }
     }
 
-    public ArrayList<float[]> getList(){
+    public ArrayList<double[]> getList(){
         return mat;
     }
 
